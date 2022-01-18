058a40e2ad90534b4b0b19883bd7b782f6dcafa9
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2022-01-18 12:01:53 +0800

    feat: Add ByteDance effect RTE extensions demo (#1)
    feat: Add ByteDance effect RTE extensions demo

---
+++ ByteDance/ios/ExtensionExample/TableViewCell.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableViewCell : UITableViewCell
+@property(weak, nonatomic) IBOutlet UITextField *sticker;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ByteDance/ios/ExtensionExample/TableViewCell.m
@@ -0,0 +1,16 @@
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+  [super awakeFromNib];
+  // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+  [super setSelected:selected animated:animated];
+
+  // Configure the view for the selected state
+}
+
+@end
