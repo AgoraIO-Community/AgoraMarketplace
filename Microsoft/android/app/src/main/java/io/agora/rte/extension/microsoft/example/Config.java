6425c297a54344a35b9eeb81e5630148b7359ba7
littleGnAl <tearjeaker@hotmail.com>
2022-03-25 15:24:22 +0800

    feat: Implement Microsoft plugin demo

---
+++ Microsoft/android/app/src/main/java/io/agora/rte/extension/microsoft/example/Config.java
@@ -0,0 +1,7 @@
+package io.agora.rte.extension.microsoft.example;
+
+public interface Config {
+    String mAppId = "<YOUR_APP_ID>";
+    String mSubscription = "<YOUR_SUBSCRIPTION>";
+    String mRegion = "<YOUR_REGION>";
+}
---
+++ Microsoft/ios/ExtensionExample/Config.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+NSString *const appID = @"<YOUR_APP_ID>";
+
+NSString *const subscription = @"<YOUR_SUBSCRIPTION>";
+
+NSString *const region = @"<YOUR_REGION>";
