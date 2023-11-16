5f4ace841ed866863a7621d57e1eaaa12cb415eb
ueishinbo <wei_xing_bo@qq.com>
2023-11-16 11:57:20 +0800

    remove irrelevant files

---
+++ FaceUnity/android/extension_demo/src/main/java/io/agora/rte/extension/faceunity/Config.java
@@ -0,0 +1,6 @@
+package io.agora.rte.extension.faceunity;
+
+public interface Config {
+    String mAppId = "1e46631b7a604c6ba31bdabb0259a58c" ; //TODO, Input your Agora AppId;
+    String mToken = null;
+}
